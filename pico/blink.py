9342c38b459453a7b67f60fbb0260c345d57c135
Lief Koepsel <lkoepsel@wellys.com>
2023-01-03 12:54:00 -0800

    init

---
+++ pico/blink.py
@@ -0,0 +1,18 @@
+# blink.py - blink a pin
+# identify the pin to blink on line 6
+from machine import Pin
+from time import sleep_ms
+
+pin = 14
+delay = 25
+freq = (1/(delay * 2))*1000
+led = Pin(pin, machine.Pin.OUT)
+
+print("blink: pin", pin, "will blink with a delay of", delay, "ms at a frequency of", freq, "kHz.")
+
+# with a delay of 100, the pin will have a 5Hz frequency
+while True:
+    led.off()
+    sleep_ms(delay)
+    led.on()
+    sleep_ms(delay)
